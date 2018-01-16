7ad2a78005cd782559d99f8c38d5dc22af7ff52f
Troy Froman <90troyfroman@gmail.com>
2018-01-16 06:55:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(array) {
+    array.unshift(name);
+    return array;
+}
+
+
